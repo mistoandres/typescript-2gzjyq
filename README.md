318b5df7cfe16c2a4f693ab944393a0242cc3f68
mistoandres <39133101+mistoandres@users.noreply.github.com>
2022-12-10 19:41:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-2gzjyq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-2gzjyq)
---
+++ index.ts
@@ -0,0 +1,8 @@
+let nombre: string = 'Strider';
+let hp: number = 95;
+
+console.log(nombre, hp);
+
+// IMPORTANTE:
+// Ver la consola de JavaScript
+
